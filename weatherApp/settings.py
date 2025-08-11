290ce6d17032a46d94cd563fe1488c893ae7a2b0
Gonzalo Novoa <85203679+chalonov@users.noreply.github.com>
2025-08-11 17:05:26 -0500

    Update settings.py

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-19qc-#uq=g&1)k@ca8qtutn*-8qo8m_02o4=y$@ye58+8q8xo4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tu-ip-publica-ec2', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['3.21.55.91', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
