ce42edc68bb9438725ac3247f758a5ec395189bd
Gonzalo Novoa <137368798+chalonov-academic@users.noreply.github.com>
2026-02-27 15:17:05 -0500

    Refactor Django settings for environment variables
    Updated settings to use environment variables for DEBUG and modified ALLOWED_HOSTS for Cloud Run compatibility. Adjusted STATIC_ROOT to use BASE_DIR.

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -23,9 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-19qc-#uq=g&1)k@ca8qtutn*-8qo8m_02o4=y$@ye58+8q8xo4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.run.app',  # Permite todos los dominios de Cloud Run
+    '*'  # Solo para desarrollo, quitar en producción real
+]
 
 
 # Application definition
@@ -118,9 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/home/ubuntu/weather-app-django-ec2/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
